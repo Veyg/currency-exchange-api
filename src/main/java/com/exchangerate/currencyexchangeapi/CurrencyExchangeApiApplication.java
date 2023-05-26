50c8b81cb8ece05442de39defe92137b0f59e539
Dawid Grochowski <96694367+Veyg@users.noreply.github.com>
2023-05-26 09:48:35 +0200

    Test

--- src/main/java/com/exchangerate/currencyexchangeapi/CurrencyExchangeApiApplication.java
+++ src/main/java/com/exchangerate/currencyexchangeapi/CurrencyExchangeApiApplication.java
@@ -3,11 +3,13 @@ package com.exchangerate.currencyexchangeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CurrencyExchangeApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeApiApplication.class, args);
+		// test
 	}
 
 }
