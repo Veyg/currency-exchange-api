51a0674b5caf33e3b8b9928381600d63d3a7986a
Dawid Grochowski <dawidgrochowski@MacBook-Air-Dawid.local>
2023-05-26 02:04:23 +0200

    Initial commit

---
+++ src/test/java/com/exchangerate/currencyexchangeapi/CurrencyExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangerate.currencyexchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
