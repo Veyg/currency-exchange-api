d221ab7cf515bb5bd027ca47048586feb87073ee
Dawid Grochowski <96694367+Veyg@users.noreply.github.com>
2023-08-09 10:44:31 +0200

    Deployment changes

--- deployment/scripts/application_start.sh
+++ deployment/scripts/application_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Start the Java API application
-
+echo "Loading env variables"
+source .env
 echo "Starting the currency-exchange-api application"
 java -jar /opt/currency-exchange-api/currency-exchange-api-1.0.0.jar
 
