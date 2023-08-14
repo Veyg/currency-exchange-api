32114fe92dca4a267341a2d24bb6d36b31037b3c
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-08-14 23:39:11 +0200

    Deployment changes for CI/CD

--- deployment/scripts/before_install.sh
+++ deployment/scripts/before_install.sh
@@ -6,3 +6,4 @@ docker rm currency-api-container || true
 
 # # Remove the deployment directory (if it exists)
 rm -rf /opt/currency-exchange-api || true
+rm -rf /opt/codedeploy-agent/deployment-root/f758f175-2dce-4cac-a31d-6aa47404eafb || true
