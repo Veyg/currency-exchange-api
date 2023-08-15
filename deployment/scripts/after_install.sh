3ed4388470458d8720387f74f3f2e483de0ed3e7
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-08-15 12:16:12 +0200

    Deployment changes for CI/CD

--- deployment/scripts/after_install.sh
+++ deployment/scripts/after_install.sh
@@ -3,3 +3,4 @@ set -e
 
 # Any additional post-installation tasks you need
 rm -rf /opt/codedeploy-agent/deployment-root/f758f175-2dce-4cac-a31d-6aa47404eafb || true
+rm -rf /opt/currency-exchange-api/
